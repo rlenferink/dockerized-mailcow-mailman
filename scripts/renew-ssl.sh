c38d2d3542603300502d3f74cbf9d678679f48f5
Roy Lenferink <lenferinkroy@gmail.com>
2023-03-05 15:30:55 +0100

    Adaptations for my setup

--- scripts/renew-ssl.sh
+++ scripts/renew-ssl.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd /opt/mailcow-dockerized && docker-compose down
+
+# Request a new certificate
 certbot renew --pre-hook "systemctl stop apache2" --post-hook "systemctl start apache2" --renew-hook "systemctl reload apache2" --quiet
-cp /etc/letsencrypt/live/MAILCOW_HOSTNAME/fullchain.pem ./data/assets/ssl/cert.pem
-cp /etc/letsencrypt/live/MAILCOW_HOSTNAME/privkey.pem ./data/assets/ssl/key.pem
-docker-compose up -d
+
+# Ensure to load the new certificates to mailcow
+cd /srv/mail/mailcow-dockerized
+cp /etc/letsencrypt/live/mail.roylenferink.com/fullchain.pem ./data/assets/ssl/cert.pem
+cp /etc/letsencrypt/live/mail.roylenferink.com/privkey.pem ./data/assets/ssl/key.pem
+
+# TODO: This can be improved to only restart the services if the certificates are actually changed (e.g. by comparing a checksum) instead of restarting every day
+./mailcow-compose.sh restart nginx-mailcow dovecot-mailcow postfix-mailcow
+
