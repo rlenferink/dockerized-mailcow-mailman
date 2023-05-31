a252e520881740d014c62e8f49853448dc89d065
Roy Lenferink <lenferinkroy@gmail.com>
2023-05-31 19:16:52 +0200

    Update renew-ssl.sh

--- scripts/renew-ssl.sh
+++ scripts/renew-ssl.sh
@@ -7,6 +7,8 @@
 cd /srv/mail/mailcow-dockerized
 cp /etc/letsencrypt/live/mail.roylenferink.com/fullchain.pem ./data/assets/ssl/cert.pem
 cp /etc/letsencrypt/live/mail.roylenferink.com/privkey.pem ./data/assets/ssl/key.pem
+cp /etc/letsencrypt/live/mail.roylenferink.com/fullchain.pem ./data/assets/ssl/server4.roylenferink.com/cert.pem
+cp /etc/letsencrypt/live/mail.roylenferink.com/privkey.pem ./data/assets/ssl/server4.roylenferink.com/key.pem
 
 # TODO: This can be improved to only restart the services if the certificates are actually changed (e.g. by comparing a checksum) instead of restarting every day
 ./mailcow-compose.sh restart nginx-mailcow dovecot-mailcow postfix-mailcow
